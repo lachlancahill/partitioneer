03cd40f472528026c62c2aa5824a93888b27f199
Lachlan Cahill <lachlan.cahill@gmail.com>
2024-09-06 17:53:41 +1200

    version name change.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='partitioneer',
-    version='0.2.1',
+    version='0.2.3',
     packages=find_packages(),
     install_requires=[
         'pandas',
