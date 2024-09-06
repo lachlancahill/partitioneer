9d88af7ee88948f8c3c05d4955b5989cd47769fb
Lachlan Cahill <lachlan.cahill@gmail.com>
2024-09-06 17:17:42 +1200

    Significant improvements to functionality, adding flexibility.

--- partitioneer/__init__.py
+++ partitioneer/__init__.py
@@ -1,1 +1,7 @@
-from .core import read_data_from_partitions, write_data_to_partitions
+from .core import (
+    PartitionFilter,
+    read_data_from_partitions,
+    write_data_to_partitions,
+    get_latest_partition_date,
+    get_first_partition_date
+)
